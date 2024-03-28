50953cf804553c4622fe46bc133139039f688a1d
Charlie Hyun <ironlove77@gmail.com>
2024-03-28 18:34:42 +0900

    feat (app) P-RAME UI 적용

--- packages/client/prame_app/lib/pages/prame_make_page.dart
+++ packages/client/prame_app/lib/pages/prame_make_page.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:prame_app/components/prame/make_prame.dart';
 import 'package:prame_app/components/prame/select_artist.dart';
 
-class PramePage extends ConsumerWidget {
+class PrameMakePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SelectArtist();
+    return MakgePrame();
   }
 }
