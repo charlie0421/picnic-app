06b826794ee13c375acc8bf54e4429197d1f76ca
Charlie Hyun <ironlove77@gmail.com>
2024-04-09 19:44:01 +0900

    feat (app) 한국어 기준 국제화

--- prame_app/lib/generated/intl/messages_ko.dart
+++ prame_app/lib/generated/intl/messages_ko.dart
@@ -22,13 +22,13 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "label_celeb_recommend": MessageLookupByLibrary.simpleMessage("유명인 추천"),
+        "label_celeb_recommend": MessageLookupByLibrary.simpleMessage("샐럽 추천"),
         "label_current_language": MessageLookupByLibrary.simpleMessage("현재 언어"),
-        "label_find_celeb": MessageLookupByLibrary.simpleMessage("더 많은 유명인 찾기"),
+        "label_find_celeb": MessageLookupByLibrary.simpleMessage("더 많은 샐럽 찾기"),
         "label_moveto_celeb_gallery":
-            MessageLookupByLibrary.simpleMessage("셀러브리티 갤러리로 이동"),
+            MessageLookupByLibrary.simpleMessage("샐럽 갤러리로 이동"),
         "label_no_celeb":
-            MessageLookupByLibrary.simpleMessage("아직 북마크한 유명인이 없습니다!"),
+            MessageLookupByLibrary.simpleMessage("아직 북마크한 샐럽이 없습니다!"),
         "lable_my_celeb": MessageLookupByLibrary.simpleMessage("내 셀럽"),
         "mypage_comment": MessageLookupByLibrary.simpleMessage("댓글관리"),
         "mypage_language": MessageLookupByLibrary.simpleMessage("언어설정"),
@@ -42,11 +42,11 @@ class MessageLookup extends MessageLookupByLibrary {
         "nav_purchases": MessageLookupByLibrary.simpleMessage("구매"),
         "text_ads_random":
             MessageLookupByLibrary.simpleMessage("광고 보기 및 무작위 이미지 수집."),
-        "text_hint_search": MessageLookupByLibrary.simpleMessage("유명인을 검색하세요."),
+        "text_hint_search": MessageLookupByLibrary.simpleMessage("샐럽을 검색하세요."),
         "text_moveto_celeb_gallery":
-            MessageLookupByLibrary.simpleMessage("선택한 셀러브리티의 집으로 이동합니다."),
+            MessageLookupByLibrary.simpleMessage("선택한 샐럽의 집으로 이동합니다."),
         "title_select_language": MessageLookupByLibrary.simpleMessage("언어 선택"),
         "toast_max_5_celeb":
-            MessageLookupByLibrary.simpleMessage("내 셀러브리티를 최대 5개까지 추가할 수 있습니다.")
+            MessageLookupByLibrary.simpleMessage("내 샐럽을 최대 5개까지 추가할 수 있습니다.")
       };
 }
