a2cc86c33d1b5a86655ccbe856834a3c679dec64
Charlie Hyun <ironlove77@gmail.com>
2024-04-15 20:16:09 +0900

    feat (app) PRAME 에 북마크 샐럽만 표출

--- prame_app/lib/components/prame/select_artist.dart
+++ prame_app/lib/components/prame/select_artist.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:prame_app/providers/celeb_list_provider.dart';
+import 'package:prame_app/providers/my_celeb_list_provider.dart';
 import 'package:prame_app/providers/prame_provider.dart';
 
 class SelectArtist extends ConsumerStatefulWidget {
@@ -24,20 +26,12 @@ class _SelectArtistState extends ConsumerState<SelectArtist> {
     return Column(
       children: [
         Container(
-          height: 116,
-          padding: EdgeInsets.only(
-            left: 36.w,
-            top: 16.h,
-          ),
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemCount: 5,
-            separatorBuilder: (context, index) => SizedBox(width: 16),
-            itemBuilder: (context, index) {
-              return _buildSelectArtist(index);
-            },
-          ),
-        ),
+            height: 116,
+            padding: EdgeInsets.only(
+              left: 36.w,
+              top: 16.h,
+            ),
+            child: _buildSelectArtist()),
         Expanded(
           child: Container(
             decoration: BoxDecoration(
@@ -50,18 +44,22 @@ class _SelectArtistState extends ConsumerState<SelectArtist> {
               mainAxisSize: MainAxisSize.max,
               children: [
                 SizedBox(height: 20.h),
-                Container(
-                  height: 110.h,
-                  padding: EdgeInsets.only(left: 36.w),
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 5,
-                    separatorBuilder: (context, index) => SizedBox(width: 16.w),
-                    itemBuilder: (context, index) {
-                      return _buildSelectPrame(index);
-                    },
-                  ),
-                ),
+                Consumer(builder:
+                    (BuildContext context, WidgetRef ref, Widget? child) {
+                  return Container(
+                    height: 110.h,
+                    padding: EdgeInsets.only(left: 36.w),
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      separatorBuilder: (context, index) =>
+                          SizedBox(width: 16.w),
+                      itemBuilder: (context, index) {
+                        return _buildSelectPrame(index);
+                      },
+                    ),
+                  );
+                }),
                 SizedBox(height: 10.h),
                 Container(
                     width: 180.w,
@@ -101,106 +99,6 @@ class _SelectArtistState extends ConsumerState<SelectArtist> {
         ),
       ],
     );
-
-    // return Stack(
-    //   children: [
-    //     if (_userImage != null)
-    //       Container(
-    //         decoration: BoxDecoration(
-    //           image: DecorationImage(
-    //             image: FileImage(_userImage!),
-    //             fit: BoxFit.cover,
-    //           ),
-    //         ),
-    //       ),
-    //     Container(
-    //         alignment: Alignment.bottomRight,
-    //         child: Image.asset('assets/mockup/prame/고윤정 고화질3 1.png')),
-    //     Row(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: [
-    //         ElevatedButton(
-    //           onPressed: getImage,
-    //           child: Text('사진첩'),
-    //         ),
-    //         if (cameras != null && cameras!.isNotEmpty) SizedBox(width: 16),
-    //         if (cameras != null && cameras!.isNotEmpty)
-    //           ElevatedButton(
-    //             onPressed: () {
-    //               showModalBottomSheet(
-    //                 context: context,
-    //                 isScrollControlled: true,
-    //                 builder: (BuildContext context) {
-    //                   return StatefulBuilder(builder:
-    //                       (BuildContext context, StateSetter setState) {
-    //                     logger.i('build');
-    //                     return SizedBox(
-    //                       height: MediaQuery.of(context)
-    //                           .size
-    //                           .height, // 전체 화면 높이를 사용
-    //                       child: Stack(
-    //                         alignment: Alignment.center,
-    //                         children: [
-    //                           CameraPreview(
-    //                             controller!,
-    //                             child: CustomPaint(
-    //                               size: Size.infinite,
-    //                               painter: OverlayImagePainter(
-    //                                   overlayImage: _overlayImage),
-    //                             ),
-    //                           ),
-    //                           Container(
-    //                             width: double.infinity,
-    //                             alignment: Alignment.bottomCenter,
-    //                             padding: const EdgeInsets.only(bottom: 100),
-    //                             child: Row(
-    //                               mainAxisAlignment: MainAxisAlignment.center,
-    //                               children: [
-    //                                 ElevatedButton(
-    //                                   onPressed: () async {
-    //                                     final XFile file =
-    //                                         await controller!.takePicture();
-    //                                     final img.Image image = img.decodeImage(
-    //                                         await file.readAsBytes())!;
-    //                                     final uiImage =
-    //                                         await _convertImage(image);
-    //                                     setState(() {
-    //                                       _userImage = File(file.path);
-    //                                       _convertedUserImage = uiImage;
-    //                                     });
-    //                                     Navigator.pop(context);
-    //                                   },
-    //                                   child: Text('사진 찍기'),
-    //                                 ),
-    //                                 SizedBox(width: 16),
-    //                                 if (cameras != null && cameras!.isNotEmpty)
-    //                                   ElevatedButton(
-    //                                     onPressed: () async {
-    //                                       _toggleCamera();
-    //                                       Future.delayed(
-    //                                           Duration(milliseconds: 1000), ()
-    //                                       {
-    //                                         setState(() {});
-    //                                       });
-    //                                     },
-    //                                     child: Text('카메라 전환'),
-    //                                   ),
-    //                               ],
-    //                             ),
-    //                           ),
-    //                         ],
-    //                       ),
-    //                     );
-    //                   });
-    //                 },
-    //               );
-    //             },
-    //             child: Text('카메라'),
-    //           ),
-    //       ],
-    //     )
-    //   ],
-    // );
   }
 
   _buildSelectPrame(int index) {
@@ -226,31 +124,52 @@ class _SelectArtistState extends ConsumerState<SelectArtist> {
     );
   }
 
-  List<String> artistList = ['이도현', '고윤정', '한소희', '김우빈', '문상민'];
-
-  _buildSelectArtist(int index) {
-    return Container(
-        height: 84,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-        ),
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/mockup/prame/prame${index + 1}.png',
-              width: 60.w,
-              height: 60.h,
-            ),
-            SizedBox(height: 8.h),
-            Text(
-              artistList[index],
-              style: Theme.of(context)
-                  .textTheme
-                  .titleMedium
-                  ?.copyWith(fontWeight: FontWeight.w700),
-            )
-          ],
-        ));
+  _buildSelectArtist() {
+    final asyncMyCelebState = ref.watch(asyncMyCelebListProvider);
+    return asyncMyCelebState.when(
+      data: (data) {
+        return Container(
+          height: 84,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: data.items.length,
+            separatorBuilder: (context, index) => SizedBox(width: 16.w),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                width: 60.w,
+                height: 60.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Image.network(
+                      data.items[index].thumbnail,
+                      width: 60.w,
+                      height: 60.h,
+                    ),
+                    SizedBox(height: 4.h),
+                    Text(
+                      data.items[index].nameKo,
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium
+                          ?.copyWith(fontWeight: FontWeight.w700),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      },
+      loading: () => CircularProgressIndicator(),
+      error: (error, stackTrace) => Text('error'),
+    );
   }
 }
