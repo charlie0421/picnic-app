40e35c2b76c5dadb7b2f3e59db458d5dc70d1fd8
Charlie Hyun <ironlove77@gmail.com>
2024-04-22 23:03:19 +0900

    feat (app) 화면 캡쳐 방지 기능 추가

--- packages/client/prame_app/lib/prame_app.dart
+++ packages/client/prame_app/lib/prame_app.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:prame_app/generated/l10n.dart';
+import 'package:prame_app/overlays.dart';
 import 'package:prame_app/providers/app_setting_provider.dart';
 import 'package:prame_app/providers/celeb_list_provider.dart';
 import 'package:prame_app/screens/draw_image_screen.dart';
@@ -13,6 +15,8 @@ import 'package:prame_app/screens/language_screen.dart';
 import 'package:prame_app/screens/my_screen.dart';
 import 'package:prame_app/screens/prame_screen.dart';
 import 'package:prame_app/ui/theme.dart';
+import 'package:prame_app/util.dart';
+import 'package:screen_protector/screen_protector.dart';
 
 class PrameApp extends ConsumerStatefulWidget {
   const PrameApp({super.key});
@@ -21,13 +25,44 @@ class PrameApp extends ConsumerStatefulWidget {
   createState() => _PrameAppState();
 }
 
-class _PrameAppState extends ConsumerState<PrameApp> {
+class _PrameAppState extends ConsumerState<PrameApp>
+    with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await ref.read(appSettingProvider.notifier).loadSettings();
+      await ScreenProtector.preventScreenshotOn();
     });
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    ScreenProtector.preventScreenshotOff();
+
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (!isWeb() && isMobile()) {
+      if (state == AppLifecycleState.inactive) {
+        blackScreenOverlaySupport ??= showOverlay(
+          (BuildContext context, t) {
+            return Container(
+              color: blackScreenColor,
+            );
+          },
+          duration: Duration.zero,
+        );
+      } else if (state == AppLifecycleState.resumed) {
+        clearBlackScreenOverlay();
+      }
+      // }
+    }
   }
 
   @override
@@ -36,49 +71,51 @@ class _PrameAppState extends ConsumerState<PrameApp> {
     final asyncCelebListState = ref.watch(asyncCelebListProvider);
     return ScreenUtilInit(
       designSize: const Size(430, 932),
-      child: MaterialApp(
-          title: 'Prame App Demo',
-          theme: themeLight,
-          darkTheme: themeLight,
-          themeMode: appSettingState.themeMode,
-          locale: appSettingState.locale,
-          routes: {
-            LandingScreen.routeName: (context) => const LandingScreen(),
-            MyScreen.routeName: (context) => const MyScreen(),
-            LanguageScreen.routeName: (context) => const LanguageScreen(),
-            PrameScreen.routeName: (context) => const PrameScreen(),
-            DrawImageScreen.routeName: (context) => const DrawImageScreen(),
-          },
-          onGenerateRoute: (settings) {
-            if (settings.name == GalleryDetailScreen.routeName) {
-              final args = settings.arguments as GalleryDetailScreenArguments;
-              return MaterialPageRoute(
-                  builder: (context) => GalleryDetailScreen(
-                        galleryId: args.galleryId,
-                        galleryName: args.galleryName,
-                      ));
-            }
-            if (settings.name == HomeScreen.routeName) {
-              final args = settings.arguments as HomeScreenArguments;
+      child: OverlaySupport.global(
+        child: MaterialApp(
+            title: 'Prame App Demo',
+            theme: themeLight,
+            darkTheme: themeLight,
+            themeMode: appSettingState.themeMode,
+            locale: appSettingState.locale,
+            routes: {
+              LandingScreen.routeName: (context) => const LandingScreen(),
+              MyScreen.routeName: (context) => const MyScreen(),
+              LanguageScreen.routeName: (context) => const LanguageScreen(),
+              PrameScreen.routeName: (context) => const PrameScreen(),
+              DrawImageScreen.routeName: (context) => const DrawImageScreen(),
+            },
+            onGenerateRoute: (settings) {
+              if (settings.name == GalleryDetailScreen.routeName) {
+                final args = settings.arguments as GalleryDetailScreenArguments;
+                return MaterialPageRoute(
+                    builder: (context) => GalleryDetailScreen(
+                          galleryId: args.galleryId,
+                          galleryName: args.galleryName,
+                        ));
+              }
+              if (settings.name == HomeScreen.routeName) {
+                final args = settings.arguments as HomeScreenArguments;
 
-              return MaterialPageRoute(
-                  builder: (context) => HomeScreen(
-                        celebModel: args.celebModel,
-                      ));
-            }
+                return MaterialPageRoute(
+                    builder: (context) => HomeScreen(
+                          celebModel: args.celebModel,
+                        ));
+              }
 
-            return null;
-          },
-          localizationsDelegates: [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: S.delegate.supportedLocales,
-          home: HomeScreen(
-            celebModel: asyncCelebListState.value?.items.first,
-          )),
+              return null;
+            },
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            home: HomeScreen(
+              celebModel: asyncCelebListState.value?.items.first,
+            )),
+      ),
     );
   }
 }
