ddb7d5ac8e35816149384ab77b864bdb71ef9b1d
Charlie Hyun <ironlove77@gmail.com>
2024-05-27 21:58:49 +0900

    refact (app) navigation bar 리펙토링

--- picnic_app/lib/screens/community/community_home_screen.dart
+++ picnic_app/lib/screens/community/community_home_screen.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:picnic_app/components/bottom/community_navigation_bar.dart';
+import 'package:picnic_app/components/bottom/navigation_bar.dart';
 import 'package:picnic_app/components/ui/picnic-animated-switcher.dart';
+import 'package:picnic_app/menu.dart';
 
 class CommunityHomeScreen extends ConsumerWidget {
   const CommunityHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-            PicnicAnimatedSwitcher(),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: CommunityBottomNavigationBar(),
-            ),
-          ],
-        ));
+    return Stack(
+      children: [
+        PicnicAnimatedSwitcher(),
+        Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: CommonBottomNavigationBar(screenInfo: communityScreenInfo)),
+      ],
+    );
   }
 }
