f78f7845b3a8f4667f78e2f7e7057555dbddae37
Charlie Hyun <ironlove77@gmail.com>
2024-06-04 20:58:36 +0900

    fix (app) PIC Camera 다이얼로그가 아닌 전체 화면으로 전환, 최근 이미지 불러오기 기능 추가

--- picnic_app/lib/providers/navigation_provider.g.dart
+++ picnic_app/lib/providers/navigation_provider.g.dart
@@ -6,12 +6,12 @@ part of 'navigation_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$navigationInfoHash() => r'5dfcae16e35dcb726f3641cfd0bde8f5bfbde263';
+String _$navigationInfoHash() => r'dc53305201fa351c934497e595955730745a869f';
 
 /// See also [NavigationInfo].
 @ProviderFor(NavigationInfo)
 final navigationInfoProvider =
-    AutoDisposeNotifierProvider<NavigationInfo, Navigation>.internal(
+    NotifierProvider<NavigationInfo, Navigation>.internal(
   NavigationInfo.new,
   name: r'navigationInfoProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final navigationInfoProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$NavigationInfo = AutoDisposeNotifier<Navigation>;
+typedef _$NavigationInfo = Notifier<Navigation>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
