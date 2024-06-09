970e848c3c4ca1ec4be38c50a9ac2b0156188446
Charlie Hyun <ironlove77@gmail.com>
2024-06-10 01:06:24 +0900

    bump (app) app version 1.0.0+61

--- packages/client/picnic_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/client/picnic_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import audioplayers_darwin
 import file_saver
 import file_selector_macos
 import firebase_auth
@@ -22,6 +23,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
