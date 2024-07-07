37a568dfce51bc59ec2404d104fff7a38de7d8ae
Charlie Hyun <ironlove77@gmail.com>
2024-07-08 04:13:05 +0900

    fix (app) error appinfo.dart

--- packages/client/picnic_app/lib/components/appinfo.dart
+++ packages/client/picnic_app/lib/components/appinfo.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:picnic_app/components/picnic_cached_network_image.dart';
 import 'package:picnic_app/constants.dart';
 import 'package:picnic_app/providers/navigation_provider.dart';
 import 'package:picnic_app/providers/platform_info_provider.dart';
 import 'package:picnic_app/supabase_options.dart';
-import 'package:picnic_app/util.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AppInfo extends ConsumerWidget {
@@ -23,13 +21,13 @@ class AppInfo extends ConsumerWidget {
         logger.d('user: $user');
         return ListView(
           children: [
-            SizedBox(
-              width: getPlatformScreenSize(context).width / 2,
-              height: getPlatformScreenSize(context).width / 2,
-              child: PicnicCachedNetworkImage(
-                Key: user?.userMetadata?['avatar_url'] ?? '',
-              ),
-            ),
+            // SizedBox(
+            //   width: getPlatformScreenSize(context).width / 2,
+            //   height: getPlatformScreenSize(context).width / 2,
+            //   child: PicnicCachedNetworkImage(
+            //     Key: user?.userMetadata?['avatar_url'] ?? '',
+            //   ),
+            // ),
             Card(
               child: ListTile(
                 title: const Text('앱버전'),
