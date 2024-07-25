cbf2cbd2feff0b3afcc98bb763133e2e1363d800
Charlie Hyun <ironlove77@gmail.com>
2024-07-25 13:22:55 +0900

    fix (app) 빌드 오류 수정

--- picnic_app/lib/main.dart
+++ picnic_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -36,7 +35,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   final authService = AuthService();
   final isSessionRecovered = await authService.recoverSession();
