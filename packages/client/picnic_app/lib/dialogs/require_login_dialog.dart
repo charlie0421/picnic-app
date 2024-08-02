81988ad3764669758055ac3f47e9228ae3e3215d
Charlie Hyun <ironlove77@gmail.com>
2024-08-02 16:34:25 +0900

    fix (app) showRequireLoginDialog 로그 강화

--- packages/client/picnic_app/lib/dialogs/require_login_dialog.dart
+++ packages/client/picnic_app/lib/dialogs/require_login_dialog.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:picnic_app/constants.dart';
 import 'package:picnic_app/dialogs/simple_dialog.dart';
 import 'package:picnic_app/generated/l10n.dart';
 import 'package:picnic_app/screens/signup/signup_screen.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void showRequireLoginDialog({
   required BuildContext context,
 }) {
-  showSimpleDialog(
-    context: context,
-    content: S.of(context).dialog_content_login_required,
-    onOk: () => Navigator.pushNamed(context, SignUpScreen.routeName)
-        .then((value) => Navigator.of(context).pop(true)),
-    onCancel: () => Navigator.of(context).pop(false),
-  );
+  try {
+    showSimpleDialog(
+      context: context,
+      content: S.of(context).dialog_content_login_required,
+      onOk: () => Navigator.pushNamed(context, SignUpScreen.routeName)
+          .then((value) => Navigator.of(context).pop(true)),
+      onCancel: () => Navigator.of(context).pop(false),
+    );
+  } catch (e, s) {
+    logger.e(e, stackTrace: s);
+    Sentry.captureException(e, stackTrace: s);
+  }
 }
