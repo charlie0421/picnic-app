23f5fdacc1f1ca4e9e6eea5167140618a1673ec1
Charlie Hyun <ironlove77@gmail.com>
2024-08-09 01:26:52 +0900

    feat (app) 커뮤니티에 마이아티스트 노출

--- packages/client/picnic_app/lib/pages/community/community_home_page.dart
+++ packages/client/picnic_app/lib/pages/community/community_home_page.dart
@@ -1,13 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:picnic_app/components/common/avartar_container.dart';
 import 'package:picnic_app/components/common/common_banner.dart';
+import 'package:picnic_app/providers/mypage/bookmarked_artists_provider.dart';
+import 'package:picnic_app/ui/style.dart';
+import 'package:picnic_app/util/i18n.dart';
+import 'package:picnic_app/util/ui.dart';
 
-class CommunityHomePage extends StatelessWidget {
+class CommunityHomePage extends ConsumerWidget {
   const CommunityHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bookmarkedArtists = ref.watch(asyncBookmarkedArtistsProvider);
+
     return ListView(children: [
       const CommonBanner('vote_home'),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Text('My ARTISTS',
+            style: getTextStyle(AppTypo.TITLE18B, AppColors.Grey900)),
+      ),
+      SizedBox(height: 16.h),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        height: 90,
+        child: bookmarkedArtists.when(
+          data: (artists) {
+            return ListView.separated(
+              itemCount: artists.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      ProfileImageContainer(
+                        avatarUrl: artists[index].image,
+                        width: 54,
+                        height: 54,
+                        borderRadius: 54,
+                      ),
+                      const SizedBox(height: 7),
+                      Text(getLocaleTextFromJson(artists[index].name),
+                          style: getTextStyle(
+                              AppTypo.CAPTION12R, AppColors.Grey900)),
+                    ],
+                  ),
+                ],
+              ),
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(width: 14.w);
+              },
+            );
+          },
+          loading: () => buildLoadingOverlay(),
+          error: (error, stack) => Text('Error: $error'),
+        ),
+      ),
     ]);
   }
 }
