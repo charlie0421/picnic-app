76347e1b88414d7b88cc568a6204706672526617
Charlie Hyun <ironlove77@gmail.com>
2024-08-16 19:41:14 +0900

    feat (app) 세팅 메뉴명 영문화 고정

--- picnic_app/lib/generated/intl/messages_zh.dart
+++ picnic_app/lib/generated/intl/messages_zh.dart
@@ -160,7 +160,8 @@ class MessageLookup extends MessageLookupByLibrary {
         "label_mypage_notice": MessageLookupByLibrary.simpleMessage("公告"),
         "label_mypage_privacy_policy":
             MessageLookupByLibrary.simpleMessage("隐私政策"),
-        "label_mypage_setting": MessageLookupByLibrary.simpleMessage("设置"),
+        "label_mypage_setting":
+            MessageLookupByLibrary.simpleMessage("Settings"),
         "label_mypage_should_login":
             MessageLookupByLibrary.simpleMessage("请登录"),
         "label_mypage_terms_of_use":
