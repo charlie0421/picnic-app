ca67449edd401edd93257fc577ba84beca74182d
Charlie Hyun <ironlove77@gmail.com>
2024-08-25 02:51:36 +0900

    feat (app) 포탈 메뉴 UI 리펙토링

--- packages/client/picnic_app/lib/screens/portal.dart
+++ packages/client/picnic_app/lib/screens/portal.dart
@@ -26,133 +26,117 @@ class Portal extends ConsumerStatefulWidget {
 
 class _PortalState extends ConsumerState<Portal> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final currentScreen = ref.watch(navigationInfoProvider.select((value) {
-      return value.currentScreen;
-    }));
+    final currentScreen = ref
+        .watch(navigationInfoProvider.select((value) => value.currentScreen));
     final userInfoState = ref.watch(userInfoProvider);
     return Container(
-        decoration: const BoxDecoration(
-          gradient: commonGradient,
+      decoration: const BoxDecoration(
+        gradient: commonGradient,
+      ),
+      child: Scaffold(
+        drawerEnableOpenDragGesture: false,
+        drawer: const Drawer(
+          width: double.infinity,
+          child: MyPageScreen(),
         ),
-        child: Scaffold(
-            drawerEnableOpenDragGesture: false,
-            drawer: const Drawer(
-              width: double.infinity,
-              child: MyPageScreen(),
-            ),
-            appBar: AppBar(
-              toolbarHeight: ref.watch(navigationInfoProvider
-                      .select((value) => value.showTopMenu))
-                  ? 56.h
-                  : 0,
-              leading: Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                    width: 24.h,
-                    height: 24,
-                    alignment: Alignment.center,
-                    child: supabase.isLogged
-                        ? userInfoState.when(
-                            data: (data) => data != null
-                                ? GestureDetector(
-                                    behavior: HitTestBehavior.opaque,
-                                    onTap: () {
-                                      Scaffold.of(context).openDrawer();
-                                    },
-                                    child: ProfileImageContainer(
-                                      avatarUrl: data.avatar_url,
-                                      width: 36.w,
-                                      height: 36,
-                                      borderRadius: 8.r,
-                                    ))
-                                : const DefaultAvatar(),
-                            error: (error, stackTrace) =>
-                                const Icon(Icons.error),
-                            loading: () => SizedBox(
-                              width: 36.w,
-                              child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.r),
-                                  child: buildPlaceholderImage()),
-                            ),
-                          )
-                        : GestureDetector(
-                            behavior: HitTestBehavior.opaque,
-                            onTap: () => Scaffold.of(context).openDrawer(),
-                            child: const DefaultAvatar(),
-                          ),
-                  );
-                },
-              ),
-              leadingWidth: 52.w,
-              titleSpacing: 0,
-              title: SizedBox(
-                height: 26,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    if (Environment.currentEnvironment != 'prod')
-                      Text(
-                        Environment.currentEnvironment,
+        appBar: AppBar(
+          toolbarHeight: ref.watch(
+                  navigationInfoProvider.select((value) => value.showTopMenu))
+              ? 56.h
+              : 0,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: 24,
+                height: 24,
+                alignment: Alignment.center,
+                child: supabase.isLogged
+                    ? userInfoState.when(
+                        data: (data) => data != null
+                            ? GestureDetector(
+                                behavior: HitTestBehavior.opaque,
+                                onTap: () {
+                                  Scaffold.of(context).openDrawer();
+                                },
+                                child: ProfileImageContainer(
+                                  avatarUrl: data.avatar_url,
+                                  width: 36.w,
+                                  height: 36,
+                                  borderRadius: 8.r,
+                                ))
+                            : const DefaultAvatar(),
+                        error: (error, stackTrace) => const Icon(Icons.error),
+                        loading: () => SizedBox(
+                          width: 36.w,
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.r),
+                              child: buildPlaceholderImage()),
+                        ),
+                      )
+                    : GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () => Scaffold.of(context).openDrawer(),
+                        child: const DefaultAvatar(),
                       ),
-                    SizedBox(
-                      height: 26,
-                      width: getPlatformScreenSize(context).width,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          const PortalMenuItem(portalType: PortalType.vote),
-                          supabase.isLogged
-                              ? userInfoState.when(
-                                  data: (userInfo) {
-                                    if (userInfo != null && userInfo.is_admin) {
-                                      return const Row(children: [
-                                        PortalMenuItem(
-                                            portalType: PortalType.pic),
-                                        PortalMenuItem(
-                                            portalType: PortalType.community),
-                                        PortalMenuItem(
-                                            portalType: PortalType.novel),
-                                      ]);
-                                    } else {
-                                      return Container();
-                                    }
-                                  },
-                                  error: (error, stackTrace) =>
-                                      const Icon(Icons.error),
-                                  loading: () => const SizedBox(),
-                                )
-                              : Container(),
-                        ],
-                      ),
+              );
+            },
+          ),
+          leadingWidth: 52.w,
+          titleSpacing: 0,
+          title: Container(
+            height: 26,
+            padding: EdgeInsets.only(right: 24.w),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  if (Environment.currentEnvironment != 'prod')
+                    Text(Environment.currentEnvironment),
+                  const PortalMenuItem(portalType: PortalType.vote),
+                  if (supabase.isLogged)
+                    userInfoState.when(
+                      data: (userInfo) {
+                        if (userInfo != null && userInfo.is_admin) {
+                          return const Row(children: [
+                            PortalMenuItem(portalType: PortalType.pic),
+                            PortalMenuItem(portalType: PortalType.community),
+                            PortalMenuItem(portalType: PortalType.novel),
+                          ]);
+                        } else {
+                          return Container();
+                        }
+                      },
+                      error: (error, stackTrace) => const Icon(Icons.error),
+                      loading: () => const SizedBox(),
                     ),
-                  ],
-                ),
+                ],
               ),
             ),
-            body: LayoutBuilder(builder: (context, constraints) {
-              return kIsWeb
-                  ? Center(
-                      child: SizedBox(
-                      width: webDesignSize.width,
-                      child: Column(children: [
-                        if (supabase.isLogged) const ScreenTop(),
-                        Expanded(child: currentScreen),
-                      ]),
-                    ))
-                  : SizedBox(
-                      width: constraints.maxWidth,
-                      height: constraints.maxHeight,
-                      child: Column(children: [
-                        if (supabase.isLogged) const ScreenTop(),
-                        Expanded(child: currentScreen),
-                      ]),
-                    );
-            })));
+          ),
+        ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return kIsWeb
+                ? Center(
+                    child: SizedBox(
+                    width: webDesignSize.width,
+                    child: Column(children: [
+                      if (supabase.isLogged) const ScreenTop(),
+                      Expanded(child: currentScreen),
+                    ]),
+                  ))
+                : SizedBox(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                    child: Column(children: [
+                      if (supabase.isLogged) const ScreenTop(),
+                      Expanded(child: currentScreen),
+                    ]),
+                  );
+          },
+        ),
+      ),
+    );
   }
 }
