ce27071f91c0082c1542aafffe73c889486179ee
Charlie Hyun <ironlove77@gmail.com>
2024-10-16 12:43:44 +0900

    feat (app) - [x] 닫기 버튼 터치영역 보정

---
+++ packages/client/picnic_app/lib/dialogs/reward_dialog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:picnic_app/components/common/reward_dialog.dart';
+import 'package:picnic_app/models/reward.dart';
+
+showRewardDialog(BuildContext context, RewardModel data) {
+  return showGeneralDialog(
+    context: context,
+    barrierDismissible: true,
+    barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+    transitionDuration: const Duration(milliseconds: 300),
+    pageBuilder: (BuildContext buildContext, Animation<double> animation,
+        Animation<double> secondaryAnimation) {
+      return Builder(
+        builder: (BuildContext context) {
+          return RewardDialog(
+            data: data,
+          );
+        },
+      );
+    },
+  );
+}
