fc326ce48ee9acd5f03512b019309150210ddf54
Charlie Hyun <ironlove77@gmail.com>
2024-10-22 00:10:59 +0900

    feat (app) 투표 완료 창 디자인 - 아티스트, 그룹의 테두리 원이 누락됨

--- picnic_app/lib/components/vote/voting/voting_complete.dart
+++ picnic_app/lib/components/vote/voting/voting_complete.dart
@@ -15,6 +15,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:picnic_app/components/common/avartar_container.dart';
 import 'package:picnic_app/components/common/picnic_cached_network_image.dart';
 import 'package:picnic_app/components/ui/large_popup.dart';
+import 'package:picnic_app/components/vote/voting/gradient_circular_progress_indicator.dart';
 import 'package:picnic_app/dialogs/simple_dialog.dart';
 import 'package:picnic_app/generated/l10n.dart';
 import 'package:picnic_app/models/vote/artist.dart';
@@ -496,13 +497,15 @@ class _VotingCompleteDialogState extends ConsumerState<VotingCompleteDialog>
 
   List<Widget> _group(ArtistGroupModel group) {
     return [
-      SizedBox(
-        width: 60,
-        height: 60,
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(120),
+          border: Border.all(color: AppColors.primary500, width: 1.5.r),
+        ),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(60.r),
           child: PicnicCachedNetworkImage(
-            imageUrl: group.image ?? '',
+            imageUrl: group.image,
             width: 60,
             height: 60,
           ),
@@ -528,143 +531,5 @@ class _VotingCompleteDialogState extends ConsumerState<VotingCompleteDialog>
           duration: _duration,
           textStyle: getTextStyle(AppTypo.caption12B, AppColors.primary500)),
     ];
-  }
-}
-
-class GradientCircularProgressIndicator extends StatefulWidget {
-  final double value;
-  final double strokeWidth;
-  final List<Color> gradientColors;
-
-  const GradientCircularProgressIndicator({
-    super.key,
-    required this.value,
-    required this.strokeWidth,
-    required this.gradientColors,
-  });
-
-  @override
-  _GradientCircularProgressIndicatorState createState() =>
-      _GradientCircularProgressIndicatorState();
-}
-
-class _GradientCircularProgressIndicatorState
-    extends State<GradientCircularProgressIndicator>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: _duration,
-    )..forward(); // 진행 후 멈춤
-
-    _animation =
-        Tween<double>(begin: 0, end: widget.value).animate(_controller);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return CustomPaint(
-          painter: _GradientCircularProgressPainter(
-            value: _animation.value,
-            strokeWidth: widget.strokeWidth,
-            gradientColors: widget.gradientColors,
-          ),
-          child: const SizedBox(
-            width: 100,
-            height: 100,
-          ),
-        );
-      },
-    );
-  }
-}
-
-class _GradientCircularProgressPainter extends CustomPainter {
-  final double value;
-  final double strokeWidth;
-  final List<Color> gradientColors;
-
-  _GradientCircularProgressPainter({
-    required this.value,
-    required this.strokeWidth,
-    required this.gradientColors,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
-    final Gradient gradient = LinearGradient(
-      colors: gradientColors,
-      stops: const [0.0, 0.75],
-    );
-
-    final Paint paint = Paint()
-      ..shader = gradient.createShader(rect)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth
-      ..strokeCap = StrokeCap.round;
-
-    final Paint backgroundPaint = Paint()
-      ..color = Colors.grey.withOpacity(0.3)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth
-      ..strokeCap = StrokeCap.round;
-
-    final Paint endPaint = Paint()
-      ..color = gradientColors.last
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth
-      ..strokeCap = StrokeCap.round;
-
-    final double radius = (size.width - strokeWidth) / 2;
-    final Offset center = Offset(size.width / 2, size.height / 2);
-    const double startAngle = -3.14159 / 2;
-    final double sweepAngle = 2 * 3.14159 * value;
-
-    canvas.drawCircle(center, radius, backgroundPaint);
-
-    if (value <= 0.75) {
-      canvas.drawArc(
-        Rect.fromCircle(center: center, radius: radius),
-        startAngle,
-        sweepAngle,
-        false,
-        paint,
-      );
-    } else {
-      canvas.drawArc(
-        Rect.fromCircle(center: center, radius: radius),
-        startAngle,
-        2 * 3.14159 * 0.75,
-        false,
-        paint,
-      );
-      canvas.drawArc(
-        Rect.fromCircle(center: center, radius: radius),
-        startAngle + 2 * 3.14159 * 0.75,
-        sweepAngle - 2 * 3.14159 * 0.75,
-        false,
-        endPaint,
-      );
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
   }
 }
