743b16187d79955de7785e62da982fd88bc51555
Charlie Hyun <ironlove77@gmail.com>
2024-10-28 03:47:39 +0900

    feat (app) 별사탕 투표내역 점검

--- packages/client/picnic_app/lib/components/custom_dropdown_button.dart
+++ packages/client/picnic_app/lib/components/custom_dropdown_button.dart
@@ -31,56 +31,55 @@ class CustomDropdown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.cw),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8).r,
-        border: Border.all(
-          color: AppColors.grey300,
-          width: 1,
+    return IntrinsicWidth(
+      child: Container(
+        height: 26,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.symmetric(horizontal: 16.cw),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8).r,
+          border: Border.all(
+            color: AppColors.grey300,
+            width: 1,
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          IntrinsicWidth(
-            child: DropdownButtonFormField<String>(
-              key: key,
-              value: value,
-              icon: Transform.rotate(
-                angle: 1.57,
-                child: SvgPicture.asset(
-                  'assets/icons/play_style=fill.svg',
-                  colorFilter: const ColorFilter.mode(
-                    AppColors.grey400,
-                    BlendMode.srcIn,
-                  ),
-                ),
+        child: DropdownButtonFormField<String>(
+          key: key,
+          value: value,
+          icon: Transform.rotate(
+            angle: 1.57,
+            child: SvgPicture.asset(
+              'assets/icons/play_style=fill.svg',
+              colorFilter: const ColorFilter.mode(
+                AppColors.grey400,
+                BlendMode.srcIn,
               ),
-              isDense: false,
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.symmetric(horizontal: 0),
-              ),
-              dropdownColor: AppColors.grey00,
-              borderRadius: BorderRadius.circular(8),
-              items: items.map((item) {
-                return DropdownMenuItem(
-                  alignment: Alignment.center,
-                  value: item.value,
-                  child: Text(
-                    item.text,
-                    style: value == item.value
-                        ? getTextStyle(AppTypo.caption12R, AppColors.grey700)
-                        : getTextStyle(AppTypo.caption12R, AppColors.grey400),
-                  ),
-                );
-              }).toList(),
-              onChanged: onChanged,
+              height: 16,
+              width: 16,
             ),
           ),
-        ],
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.zero,
+            isDense: true,
+          ),
+          isDense: true,
+          dropdownColor: AppColors.grey00,
+          borderRadius: BorderRadius.circular(8),
+          items: items.map((item) {
+            return DropdownMenuItem(
+              alignment: Alignment.center,
+              value: item.value,
+              child: Text(
+                item.text,
+                style: value == item.value
+                    ? getTextStyle(AppTypo.caption12R, AppColors.grey700)
+                    : getTextStyle(AppTypo.caption12R, AppColors.grey400),
+              ),
+            );
+          }).toList(),
+          onChanged: onChanged,
+        ),
       ),
     );
   }
