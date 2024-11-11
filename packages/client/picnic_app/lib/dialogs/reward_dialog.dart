b594c22eecf984b2964e581fefb93102d1f16f77
Charlie Hyun <ironlove77@gmail.com>
2024-11-11 11:02:50 +0900

    feat (app) 리워드 창 UI 개선

--- packages/client/picnic_app/lib/dialogs/reward_dialog.dart
+++ packages/client/picnic_app/lib/dialogs/reward_dialog.dart
@@ -10,26 +10,28 @@ import 'package:picnic_app/util/i18n.dart';
 import 'package:picnic_app/util/ui.dart';
 import 'package:picnic_app/util/util.dart';
 
+// 상수 정의
+class RewardDialogConstants {
+  static const double imageRadius = 24;
+  static const double topSectionHeight = 400;
+  static const double closeButtonSize = 48;
+  static const Duration transitionDuration = Duration(milliseconds: 300);
+}
+
+enum RewardType { overview, location, size_guide }
+
 showRewardDialog(BuildContext context, RewardModel data) {
   return showGeneralDialog(
     context: context,
     barrierDismissible: true,
     barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-    transitionDuration: const Duration(milliseconds: 300),
+    transitionDuration: RewardDialogConstants.transitionDuration,
     pageBuilder: (BuildContext buildContext, Animation<double> animation,
         Animation<double> secondaryAnimation) {
-      return Builder(
-        builder: (BuildContext context) {
-          return RewardDialog(
-            data: data,
-          );
-        },
-      );
+      return RewardDialog(data: data);
     },
   );
 }
-
-enum RewardType { overview, location, size_guide }
 
 class RewardDialog extends StatefulWidget {
   final RewardModel data;
@@ -43,6 +45,9 @@ class RewardDialog extends StatefulWidget {
 class _RewardDialogState extends State<RewardDialog> {
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final topPadding = mediaQuery.padding.top;
+
     return Dialog(
       insetPadding: EdgeInsets.zero,
       clipBehavior: Clip.antiAlias,
@@ -54,28 +59,40 @@ class _RewardDialogState extends State<RewardDialog> {
         height: getPlatformScreenSize(context).height,
         child: Stack(
           children: [
-            SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  buildTopSection(),
-                  const SizedBox(height: 67),
-                  buildSection(RewardType.overview),
-                  buildSection(RewardType.location),
-                  buildSection(RewardType.size_guide),
-                ],
+            Transform.translate(
+              offset: Offset(0, -topPadding),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    _buildTopSection(),
+                    const SizedBox(height: 67),
+                    ...RewardType.values.map((type) {
+                      return Column(
+                        children: [
+                          RewardSection(
+                            type: type,
+                            data: widget.data,
+                          ),
+                          if (type != RewardType.values.last)
+                            const SizedBox(height: 68),
+                        ],
+                      );
+                    }),
+                  ],
+                ),
               ),
             ),
             Positioned(
-              top: 40,
+              top: 40 + topPadding,
               right: 15.cw,
               child: GestureDetector(
                 onTap: () => Navigator.of(context).pop(),
                 child: SvgPicture.asset(
                   'assets/icons/cancel_style=fill.svg',
-                  width: 48,
-                  height: 48,
+                  width: RewardDialogConstants.closeButtonSize,
+                  height: RewardDialogConstants.closeButtonSize,
                   colorFilter: ColorFilter.mode(
                     AppColors.grey200.withOpacity(0.8),
                     BlendMode.srcIn,
@@ -89,141 +106,207 @@ class _RewardDialogState extends State<RewardDialog> {
     );
   }
 
-  Widget buildTopSection() {
+  Widget _buildTopSection() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: [
+          SizedBox.expand(
+            child: ShaderMask(
+              shaderCallback: (rect) => const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black, Colors.transparent],
+                stops: [0.7, 1],
+              ).createShader(rect),
+              blendMode: BlendMode.dstIn,
+              child: PicnicCachedNetworkImage(
+                imageUrl: widget.data.thumbnail ?? '',
+                fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width.toInt(),
+                height: MediaQuery.of(context).size.width.toInt(),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 48,
+              margin: EdgeInsets.symmetric(horizontal: 30.cw),
+              child: VoteCommonTitle(
+                title: getLocaleTextFromJson(widget.data.title!),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RewardSection extends StatelessWidget {
+  final RewardType type;
+  final RewardModel data;
+
+  const RewardSection({
+    super.key,
+    required this.type,
+    required this.data,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       children: [
-        SizedBox(
-          width: getPlatformScreenSize(context).width,
-          child: ShaderMask(
-            shaderCallback: (rect) => const LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.black, Colors.transparent],
-              stops: [0.7, 1],
-            ).createShader(rect),
-            blendMode: BlendMode.dstIn,
-            child: PicnicCachedNetworkImage(
-              imageUrl: widget.data.thumbnail ?? '',
-              width: 400,
-              fit: BoxFit.fill,
-            ),
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(horizontal: 16.cw, vertical: 12),
+          padding: EdgeInsets.symmetric(horizontal: 24.cw, vertical: 53),
+          decoration: BoxDecoration(
+            borderRadius:
+                BorderRadius.circular(RewardDialogConstants.imageRadius),
+            border: Border.all(color: AppColors.primary500, width: 1.5),
           ),
+          child: Column(children: _buildSectionContent(context)),
         ),
         Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            height: 48,
-            margin: EdgeInsets.symmetric(horizontal: 30.cw),
-            child: VoteCommonTitle(
-                title: getLocaleTextFromJson(widget.data.title!)),
+          top: 0,
+          left: 40.cw,
+          child: Image.asset(
+            'assets/images/reward_${type.name}.png',
+            height: 24,
           ),
         ),
       ],
     );
   }
 
-  Widget buildSection(RewardType type) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.symmetric(horizontal: 16.cw, vertical: 12),
-              padding: EdgeInsets.symmetric(horizontal: 24.cw, vertical: 53),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(24.r),
-                border: Border.all(color: AppColors.primary500, width: 1.5.r),
-              ),
-              child: Column(children: buildSectionContent(type)),
-            ),
-            Positioned(
-              top: 0,
-              left: 40.cw,
-              child: Image.asset('assets/images/reward_${type.name}.png',
-                  height: 24),
-            ),
-          ],
-        ),
-        const SizedBox(height: 68),
-      ],
-    );
-  }
-
-  List<Widget> buildSectionContent(RewardType type) {
+  List<Widget> _buildSectionContent(BuildContext context) {
     final locale = Intl.getCurrentLocale();
+    List<Widget> widgets = [];
+
     switch (type) {
       case RewardType.overview:
-        return buildImageList(widget.data.overviewImages);
+        if (data.overviewImages != null) {
+          widgets.addAll(_buildImageList(context, data.overviewImages));
+        }
+        break;
+
       case RewardType.location:
-        return [
-          ...buildImageList(
-              widget.data.location?[locale]['map'].cast<String>(),
-              BoxDecoration(
-                borderRadius: BorderRadius.circular(24.r),
-                border: Border.all(
+        if (data.location?[locale] != null) {
+          final locationData = data.location![locale];
+          // Map 이미지 처리
+          if (locationData['map'] != null) {
+            final mapImages = locationData['map'].cast<String>();
+            if (mapImages.isNotEmpty) {
+              widgets.addAll(_buildImageList(
+                context,
+                [mapImages.first],
+                BoxDecoration(
+                  borderRadius:
+                      BorderRadius.circular(RewardDialogConstants.imageRadius),
+                  border: Border.all(
                     color: AppColors.primary500,
-                    width: 3.r,
-                    strokeAlign: BorderSide.strokeAlignInside),
-              )).sublist(0, 1),
-          ...buildImageList(widget.data.location?[locale]['map'].cast<String>())
-              .sublist(1),
-          const SizedBox(height: 24),
-          ...buildTextAddress(
-              widget.data.location?[locale]['address'].cast<String>(),
-              getTextStyle(AppTypo.body16B, AppColors.grey900)),
-          const SizedBox(height: 24),
-          if (widget.data.location?[locale]['images'] != null)
-            ...buildImageList(
-                widget.data.location?[locale]['images'].cast<String>()),
-          const SizedBox(height: 24),
-          ...buildTextList(widget.data.location?[locale]['desc'].cast<String>(),
-              getTextStyle(AppTypo.body16B, AppColors.grey900)),
-        ];
+                    width: 3,
+                    strokeAlign: BorderSide.strokeAlignInside,
+                  ),
+                ),
+              ));
+              if (mapImages.length > 1) {
+                widgets.addAll(_buildImageList(context, mapImages.sublist(1)));
+              }
+            }
+          }
+
+          if (locationData['address'] != null) {
+            widgets.add(const SizedBox(height: 24));
+            widgets.addAll(_buildTextAddress(
+              context,
+              locationData['address'].cast<String>(),
+              getTextStyle(AppTypo.body16B, AppColors.grey900),
+            ));
+          }
+
+          if (locationData['images'] != null) {
+            widgets.add(const SizedBox(height: 24));
+            widgets.addAll(_buildImageList(
+              context,
+              locationData['images'].cast<String>(),
+            ));
+          }
+
+          if (locationData['desc'] != null) {
+            widgets.add(const SizedBox(height: 24));
+            widgets.addAll(_buildTextList(
+              locationData['desc'].cast<String>(),
+              getTextStyle(AppTypo.body16B, AppColors.grey900),
+            ));
+          }
+        }
+        break;
+
       case RewardType.size_guide:
-        return widget.data.sizeGuide?[locale]?.map<Widget>((value) {
-              return Column(
-                children: [
-                  ...buildSizeGuideImageList(value['image'].cast<String>()),
-                  const SizedBox(height: 24),
-                  ...buildTextList(value['desc'].cast<String>().sublist(0, 1),
-                      getTextStyle(AppTypo.body16B, AppColors.grey900)),
-                  ...buildTextList(value['desc'].cast<String>().sublist(1),
-                      getTextStyle(AppTypo.body16R, AppColors.grey900)),
-                  const SizedBox(height: 24),
-                ],
-              );
-            }).toList() ??
-            [];
-      default:
-        return [];
+        if (data.sizeGuide?[locale] != null) {
+          for (final guide in data.sizeGuide![locale]) {
+            if (guide['image'] != null) {
+              widgets.addAll(_buildSizeGuideImageList(
+                context,
+                guide['image'].cast<String>(),
+              ));
+            }
+
+            if (guide['desc'] != null) {
+              widgets.add(const SizedBox(height: 24));
+              final descList = guide['desc'].cast<String>();
+              if (descList.isNotEmpty) {
+                widgets.addAll(_buildTextList(
+                  [descList.first],
+                  getTextStyle(AppTypo.body16B, AppColors.grey900),
+                ));
+                if (descList.length > 1) {
+                  widgets.addAll(_buildTextList(
+                    descList.sublist(1),
+                    getTextStyle(AppTypo.body16R, AppColors.grey900),
+                  ));
+                }
+              }
+            }
+            widgets.add(const SizedBox(height: 24));
+          }
+        }
+        break;
     }
+
+    return widgets;
   }
 
-  List<Widget> buildImageList(List<String>? images,
-      [BoxDecoration? decoration]) {
+  List<Widget> _buildImageList(
+    BuildContext context,
+    List<String>? images, [
+    BoxDecoration? decoration,
+  ]) {
     if (images == null) return [];
+
+    final imageSize = MediaQuery.of(context).size.width - 100;
+
     return images.asMap().entries.map((entry) {
-      int i = entry.key;
-      String image = entry.value;
+      final i = entry.key;
+      final image = entry.value;
       return Column(
         children: [
           Container(
             decoration: decoration,
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(24.r),
-              child: Container(
-                alignment: Alignment.topCenter,
-                width: MediaQuery.of(context).size.width - 100,
-                height: MediaQuery.of(context).size.width - 100,
-                child: PicnicCachedNetworkImage(
-                  imageUrl: image,
-                  width: (MediaQuery.of(context).size.width - 100).toInt(),
-                  height: (MediaQuery.of(context).size.width - 100).toInt(),
-                  fit: BoxFit.cover,
-                ),
+              borderRadius:
+                  BorderRadius.circular(RewardDialogConstants.imageRadius),
+              child: PicnicCachedNetworkImage(
+                imageUrl: image,
+                width: imageSize.toInt(),
+                height: imageSize.toInt(),
+                fit: BoxFit.cover,
               ),
             ),
           ),
@@ -233,11 +316,13 @@ class _RewardDialogState extends State<RewardDialog> {
     }).toList();
   }
 
-  List<Widget> buildSizeGuideImageList(List<String>? images) {
+  List<Widget> _buildSizeGuideImageList(
+      BuildContext context, List<String>? images) {
     if (images == null) return [];
+
     return images.asMap().entries.map((entry) {
-      int i = entry.key;
-      String image = entry.value;
+      final i = entry.key;
+      final image = entry.value;
       return Column(
         children: [
           SizedBox(
@@ -252,8 +337,9 @@ class _RewardDialogState extends State<RewardDialog> {
     }).toList();
   }
 
-  List<Widget> buildTextList(List<String>? texts, [TextStyle? style]) {
+  List<Widget> _buildTextList(List<String>? texts, [TextStyle? style]) {
     if (texts == null) return [];
+
     return texts
         .map((text) => Text(
               text,
@@ -263,8 +349,13 @@ class _RewardDialogState extends State<RewardDialog> {
         .toList();
   }
 
-  List<Widget> buildTextAddress(List<String>? texts, [TextStyle? style]) {
+  List<Widget> _buildTextAddress(
+    BuildContext context,
+    List<String>? texts, [
+    TextStyle? style,
+  ]) {
     if (texts == null) return [];
+
     return texts
         .map((text) => Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
