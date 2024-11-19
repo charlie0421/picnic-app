534595eb38093b6e63a2b44d41de1316d1d65e36
Charlie Hyun <ironlove77@gmail.com>
2024-11-19 23:56:19 +0900

    feat (app) 아티스트별 궁합 리스트 기능

--- picnic_app/lib/generated/providers/community/compatibility_history_provider.g.dart
+++ picnic_app/lib/generated/providers/community/compatibility_history_provider.g.dart
@@ -6,22 +6,172 @@ part of '../../../providers/community/compatibility_history_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$compatibilityHistoryHash() =>
-    r'1827db1777ec8f76981903005173cc0835ce9cfd';
+String _$compatibilityListHash() => r'508c4433ccf99cbff74f76318bb70201c335541f';
 
-/// See also [CompatibilityHistory].
-@ProviderFor(CompatibilityHistory)
-final compatibilityHistoryProvider = AutoDisposeNotifierProvider<
-    CompatibilityHistory, CompatibilityHistoryModel>.internal(
-  CompatibilityHistory.new,
-  name: r'compatibilityHistoryProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$compatibilityHistoryHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
 
-typedef _$CompatibilityHistory = AutoDisposeNotifier<CompatibilityHistoryModel>;
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$CompatibilityList
+    extends BuildlessAutoDisposeNotifier<CompatibilityHistoryModel> {
+  late final int? artistId;
+
+  CompatibilityHistoryModel build({
+    int? artistId,
+  });
+}
+
+/// See also [CompatibilityList].
+@ProviderFor(CompatibilityList)
+const compatibilityListProvider = CompatibilityListFamily();
+
+/// See also [CompatibilityList].
+class CompatibilityListFamily extends Family<CompatibilityHistoryModel> {
+  /// See also [CompatibilityList].
+  const CompatibilityListFamily();
+
+  /// See also [CompatibilityList].
+  CompatibilityListProvider call({
+    int? artistId,
+  }) {
+    return CompatibilityListProvider(
+      artistId: artistId,
+    );
+  }
+
+  @override
+  CompatibilityListProvider getProviderOverride(
+    covariant CompatibilityListProvider provider,
+  ) {
+    return call(
+      artistId: provider.artistId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'compatibilityListProvider';
+}
+
+/// See also [CompatibilityList].
+class CompatibilityListProvider extends AutoDisposeNotifierProviderImpl<
+    CompatibilityList, CompatibilityHistoryModel> {
+  /// See also [CompatibilityList].
+  CompatibilityListProvider({
+    int? artistId,
+  }) : this._internal(
+          () => CompatibilityList()..artistId = artistId,
+          from: compatibilityListProvider,
+          name: r'compatibilityListProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$compatibilityListHash,
+          dependencies: CompatibilityListFamily._dependencies,
+          allTransitiveDependencies:
+              CompatibilityListFamily._allTransitiveDependencies,
+          artistId: artistId,
+        );
+
+  CompatibilityListProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.artistId,
+  }) : super.internal();
+
+  final int? artistId;
+
+  @override
+  CompatibilityHistoryModel runNotifierBuild(
+    covariant CompatibilityList notifier,
+  ) {
+    return notifier.build(
+      artistId: artistId,
+    );
+  }
+
+  @override
+  Override overrideWith(CompatibilityList Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: CompatibilityListProvider._internal(
+        () => create()..artistId = artistId,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        artistId: artistId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeNotifierProviderElement<CompatibilityList,
+      CompatibilityHistoryModel> createElement() {
+    return _CompatibilityListProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is CompatibilityListProvider && other.artistId == artistId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, artistId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin CompatibilityListRef
+    on AutoDisposeNotifierProviderRef<CompatibilityHistoryModel> {
+  /// The parameter `artistId` of this provider.
+  int? get artistId;
+}
+
+class _CompatibilityListProviderElement
+    extends AutoDisposeNotifierProviderElement<CompatibilityList,
+        CompatibilityHistoryModel> with CompatibilityListRef {
+  _CompatibilityListProviderElement(super.provider);
+
+  @override
+  int? get artistId => (origin as CompatibilityListProvider).artistId;
+}
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
