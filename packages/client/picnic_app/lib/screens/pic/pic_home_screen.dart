463ef3bc55b55175767ef558500cf83c9fd593c4
Charlie Hyun <ironlove77@gmail.com>
2024-11-21 00:41:28 +0900

    feat (app) PIC 스와이프 처리

--- packages/client/picnic_app/lib/screens/pic/pic_home_screen.dart
+++ packages/client/picnic_app/lib/screens/pic/pic_home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:picnic_app/bottom_navigation_menu.dart';
@@ -14,6 +16,33 @@ class PicHomeScreen extends ConsumerStatefulWidget {
 }
 
 class _PicHomeScreenState extends ConsumerState<PicHomeScreen> {
+  bool _isSwipeEnabled = true;
+  Timer? _swipeTimer;
+
+  @override
+  void dispose() {
+    _swipeTimer?.cancel();
+    super.dispose();
+  }
+
+  void _handleRightSwipe() {
+    if (_isSwipeEnabled) {
+      logger.d('Right swipe detected');
+      final navigationInfoNotifier = ref.read(navigationInfoProvider.notifier);
+      navigationInfoNotifier.goBack();
+      setState(() {
+        _isSwipeEnabled = false;
+      });
+
+      _swipeTimer?.cancel();
+      _swipeTimer = Timer(const Duration(seconds: 1), () {
+        setState(() {
+          _isSwipeEnabled = true;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final showBottomNavigation = ref.watch(
@@ -23,18 +52,44 @@ class _PicHomeScreenState extends ConsumerState<PicHomeScreen> {
     logger.d('showBottomNavigation: $showBottomNavigation');
     logger.d('picBottomNavigationIndex: $picBottomNavigationIndex');
 
-    return Stack(
-      children: [
-        const PicnicAnimatedSwitcher(),
-        if (showBottomNavigation == true)
-          Positioned(
-              bottom: MediaQuery.of(context).padding.bottom + 20,
-              left: 0,
-              right: 0,
-              child: CommonBottomNavigationBar(
-                screenInfo: picScreenInfo,
-              )),
-      ],
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (
+        didPop,
+        result,
+      ) {
+        logger.d('PopScope onPopInvokedWithResult: $didPop, $result');
+        _handleRightSwipe();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: GestureDetector(
+          onPanUpdate: (details) {
+            // 수평 방향의 움직임이 수직 방향보다 크고, 오른쪽으로 움직일 때만 처리
+            if (details.delta.dx.abs() > details.delta.dy.abs() &&
+                details.delta.dx > 0) {
+              // 여기서 임계값을 설정하여 작은 움직임은 무시할 수 있습니다.
+              if (details.delta.dx > 20) {
+                // 예: 20픽셀 이상의 움직임만 고려
+                _handleRightSwipe();
+              }
+            }
+          },
+          child: Stack(
+            children: [
+              const PicnicAnimatedSwitcher(),
+              if (showBottomNavigation == true)
+                Positioned(
+                    bottom: MediaQuery.of(context).padding.bottom + 20,
+                    left: 0,
+                    right: 0,
+                    child: CommonBottomNavigationBar(
+                      screenInfo: picScreenInfo,
+                    )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
