7b271f357e2737b51c9cdab7afa07f33f1677694
Charlie Hyun <ironlove77@gmail.com>
2025-02-03 15:35:26 +0900

    bump (lib) 서버기반 가상머신 차단 로직 적용

--- picnic_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ picnic_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import flutter_inappwebview_macos
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import in_app_purchase_storekit
+import network_info_plus
 import package_info_plus
 import path_provider_foundation
 import photo_manager
@@ -38,6 +39,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
