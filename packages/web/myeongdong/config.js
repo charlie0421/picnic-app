e00658ca4e8baa163f740034e19feb67b40bdbc2
Charlie Hyun <ironlove77@gmail.com>
2025-02-19 20:59:09 +0900

    feat (pic-chart) 명동 사이니지용 정적 파일 생성

---
+++ packages/web/myeongdong/config.js
@@ -0,0 +1,9 @@
+// 환경 설정 파일 - config.js
+// 이 파일을 HTML에 먼저 로드하면, 전역 config 객체를 사용할 수 있습니다.
+const config = {
+  SUPABASE_URL: 'https://xtijtefcycoeqludlngc.supabase.co',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0aWp0ZWZjeWNvZXFsdWRsbmdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4OTEyNzQsImV4cCI6MjAzMTQ2NzI3NH0.k0Viu8kgOnkJ7-tnrDTmqpe6TdtZCYkqmH_5vUvcv_k',
+  BRANCH_KEY: 'key_live_dsiXNxlgIa3pmcn6eK0djnjmzucZNIS4',
+  CDN_URL: 'https://cdn.picnic.fan/picnic',
+};
