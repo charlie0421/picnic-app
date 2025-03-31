427bb60c786adf6d805f5c313f3c913ce43a1e80
Charlie Hyun <ironlove77@gmail.com>
2025-03-31 19:42:19 +0900

    feat (ttja) Pangle SDK 통합 및 광고 기능 추가, MainActivity 및 PangleAdManager 구현

--- packages/client/ttja_app/ios/Runner/AppDelegate.swift
+++ packages/client/ttja_app/ios/Runner/AppDelegate.swift
@@ -1,136 +1,163 @@
 import Flutter
-import UIKit
+import PAGAdSDK
 import PincruxOfferwall
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  var offerwall: PincruxOfferwallSDK?
-  
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-        let methodChannel = FlutterMethodChannel(name:"com.pincrux.offerwall.flutter", binaryMessenger: controller.binaryMessenger)
+    var offerwall: PincruxOfferwallSDK?
+    private var pangleAdManager: PangleAdManager?
+
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+
+        // Pangle 채널 생성 및 저장
+        let pangleChannel = FlutterMethodChannel(
+            name: "pangle_native_channel",
+            binaryMessenger: controller.binaryMessenger)
+
+        // Pangle 광고 매니저 초기화
+        pangleAdManager = PangleAdManager(channel: pangleChannel)
+
+        // Pangle 채널 메소드 핸들러 설정
+        pangleChannel.setMethodCallHandler { [weak self] call, result in
+            self?.pangleAdManager?.handleMethodCall(call, result: result)
+        }
+
+        let methodChannel = FlutterMethodChannel(
+            name: "com.pincrux.offerwall.flutter", binaryMessenger: controller.binaryMessenger)
 
         methodChannel.setMethodCallHandler({
             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-            
-            switch(call.method) {
+
+            switch call.method {
             case "init":
-                if let args = call.arguments as? Dictionary<String, Any>,
-                   let pubkey = args["pubkey"] as? String,
-                   let usrkey = args["usrkey"] as? String{
+                if let args = call.arguments as? [String: Any],
+                    let pubkey = args["pubkey"] as? String,
+                    let usrkey = args["usrkey"] as? String
+                {
                     self.offerwall = PincruxOfferwallSDK.initWithPubkeyAndUsrkey(pubkey, usrkey)
                 }
-                
+
             case "setOfferwallViewControllerType":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let type = args["type"] as? Int {
-                    if type==1 {
+                    let args = call.arguments as? [String: Any],
+                    let type = args["type"] as? Int
+                {
+                    if type == 1 {
                         self.offerwall?.setViewControllerType(.Modal)
-                    } else if type==2 {
+                    } else if type == 2 {
                         self.offerwall?.setViewControllerType(.ViewType)
                     }
                 }
-                
+
             case "startOfferwall":
                 if self.isOfferwallNotNil() {
                     self.offerwall?.startOfferwall(vc: controller)
                 }
-                
+
             case "startPincruxOfferwallAdDetail":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let appkey = args["appkey"] as? String {
+                    let args = call.arguments as? [String: Any],
+                    let appkey = args["appkey"] as? String
+                {
                     self.offerwall?.startOfferwallDetailVC(vc: controller, appKey: appkey)
                 }
-                
+
             case "startPincruxOfferwallContact":
-                if self.isOfferwallNotNil(){
+                if self.isOfferwallNotNil() {
                     self.offerwall?.startOfferwallContactVC(vc: controller)
                 }
-                
+
             case "setOfferwallType":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let type = args["type"] as? Int{
-                    if type==2 {
+                    let args = call.arguments as? [String: Any],
+                    let type = args["type"] as? Int
+                {
+                    if type == 2 {
                         self.offerwall?.setOfferwallType(.BAR_PREMIUM_TYPE)
-                    } else if type==3 {
+                    } else if type == 3 {
                         self.offerwall?.setOfferwallType(.PREMIUM_TYPE)
                     } else {
                         self.offerwall?.setOfferwallType(.BAR_TYPE)
                     }
                 }
-                
+
             case "setEnableTab":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let isEnable = args["isEnable"] as? Bool{
+                    let args = call.arguments as? [String: Any],
+                    let isEnable = args["isEnable"] as? Bool
+                {
                     self.offerwall?.setEnableTab(isEnable)
                 }
-                
+
             case "setOfferwallTitle":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let title = args["title"] as? String {
+                    let args = call.arguments as? [String: Any],
+                    let title = args["title"] as? String
+                {
                     self.offerwall?.setOfferwallTitle(title)
                 }
-                
+
             case "setOfferwallThemeColor":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let color = args["color"] as? String {
+                    let args = call.arguments as? [String: Any],
+                    let color = args["color"] as? String
+                {
                     self.offerwall?.setThemeColor(color)
                 }
-                
+
             case "setEnableScrollTopButton":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let isEnable = args["isEnable"] as? Bool{
+                    let args = call.arguments as? [String: Any],
+                    let isEnable = args["isEnable"] as? Bool
+                {
                     self.offerwall?.setEnableScrollTopButton(isEnable)
                 }
-                
+
             case "setAdDetail":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let isEnable = args["isEnable"] as? Bool{
+                    let args = call.arguments as? [String: Any],
+                    let isEnable = args["isEnable"] as? Bool
+                {
                     self.offerwall?.setAdDetail(isEnable)
                 }
-                
+
             case "setDisableCPS":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let isDisable = args["isDisable"] as? Bool{
+                    let args = call.arguments as? [String: Any],
+                    let isDisable = args["isDisable"] as? Bool
+                {
                     self.offerwall?.setDisableCPS(isDisable)
                 }
-                
+
             case "setDarkMode":
                 if self.isOfferwallNotNil(),
-                   let args = call.arguments as? Dictionary<String, Any>,
-                   let mode = args["mode"] as? Int{
-                    
-                    if mode==0 {
+                    let args = call.arguments as? [String: Any],
+                    let mode = args["mode"] as? Int
+                {
+                    if mode == 0 {
                         self.offerwall?.setDarkMode(.AUTO)
-                    } else if mode==2 {
+                    } else if mode == 2 {
                         self.offerwall?.setDarkMode(.DARK_ONLY)
                     } else {
                         self.offerwall?.setDarkMode(.LIGHT_ONLY)
                     }
                 }
-                
+
             default:
                 result(FlutterMethodNotImplemented)
             }
         })
-        
+
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-        private func isOfferwallNotNil() -> Bool {
+    private func isOfferwallNotNil() -> Bool {
         if self.offerwall != nil {
             return true
         } else {
