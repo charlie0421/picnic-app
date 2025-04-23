25b02b6cc50674989fd70cb260856a6418bf7fac
Charlie Hyun <ironlove77@gmail.com>
2025-04-23 23:13:44 +0900

    feat (app) 로케일 상태 제공자 추가: 로케일 상태를 관리하는 LocaleState 클래스를 구현하고, 디바이스 로케일 및 저장된 로케일을 기반으로 초기화 및 업데이트 기능을 추가함. 자동 해시 생성 및 Crowdin 번역 로드 기능도 포함됨.

---
+++ packages/client/picnic_lib/lib/generated/providers/locale_state_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../../presentation/providers/locale_state_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$localeStateHash() => r'c2e2e0b2e5513dbee4587c4bff3dee658c38759d';
+
+/// 로케일 상태 제공자
+///
+/// Copied from [LocaleState].
+@ProviderFor(LocaleState)
+final localeStateProvider =
+    AutoDisposeNotifierProvider<LocaleState, Locale>.internal(
+  LocaleState.new,
+  name: r'localeStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$localeStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LocaleState = AutoDisposeNotifier<Locale>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
