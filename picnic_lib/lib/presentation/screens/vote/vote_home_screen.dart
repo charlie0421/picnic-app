9ff800c5143ad56b153a3ec927cc5a142da5a534
Charlie Hyun <ironlove77@gmail.com>
2025-05-14 00:36:10 +0900

    feat (picnic-app) 포털 화면에 팝업 캐러셀 추가: 웹 및 모바일 레이아웃에 맞춰 메인 콘텐츠와 함께 팝업 캐러셀을 스택으로 배치하여 사용자 경험을 향상시킴. VoteHomeScreen에서 팝업 캐러셀 관련 코드 제거.

--- picnic_lib/lib/presentation/screens/vote/vote_home_screen.dart
+++ picnic_lib/lib/presentation/screens/vote/vote_home_screen.dart
@@ -15,7 +15,6 @@ import 'package:picnic_lib/presentation/pages/vote/vote_home_page.dart';
 import 'package:picnic_lib/presentation/pages/vote/pic_chart_page.dart';
 import 'package:picnic_lib/presentation/pages/vote/vote_media_list_page.dart';
 import 'package:picnic_lib/presentation/pages/vote/store_page.dart';
-import 'package:picnic_lib/presentation/widgets/ui/popup_carousel.dart';
 
 class VoteHomeScreen extends ConsumerStatefulWidget {
   const VoteHomeScreen({super.key});
@@ -129,7 +128,6 @@ class _VoteHomeScreenState extends ConsumerState<VoteHomeScreen> {
                 right: 0,
                 child: CommonBottomNavigationBar(),
               ),
-            const Center(child: PopupCarousel()),
           ],
         ),
       ),
